7e715de93fd3f931c73fddf3ba0f2618ac872f37
NonymousMorlock <59588180+NonymousMorlock@users.noreply.github.com>
2026-08-20 19:50:09 +0000

    solve `a2`

--- src/bin/a2.rs
+++ src/bin/a2.rs
@@ -8,4 +8,14 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn main() {
+    display_sum(add, 1, 2)
+}
+
+fn display_sum(function: fn(n1: i32, n2: i32) -> i32, param_1: i32, param_2: i32) {
+    println!("{:?} + {:?} = {:?}", param_1, param_2, function(param_1, param_2))
+}
+
+fn add(num_1: i32, num_2: i32) -> i32 {
+    num_1 + num_2
+}
