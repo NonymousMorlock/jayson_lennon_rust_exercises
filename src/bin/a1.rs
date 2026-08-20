5401617bbeeff58c37bfa7ff8a76273959407deb
NonymousMorlock <59588180+NonymousMorlock@users.noreply.github.com>
2026-08-20 19:49:48 +0000

    solve `a1`

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -8,4 +8,14 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+   println!("First Name: {:?};\nLast Name: {:?}", first_name(), last_name());
+}
+
+fn first_name() -> String {
+   "John".to_string()
+}
+
+fn last_name() -> String {
+   "Smith".to_string()
+}
