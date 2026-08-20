155ad739f724fc319cf0ef5463e4b7461844ce54
NonymousMorlock <59588180+NonymousMorlock@users.noreply.github.com>
2026-08-20 19:50:45 +0000

    solve `a3a`

--- src/bin/a3a.rs
+++ src/bin/a3a.rs
@@ -12,4 +12,16 @@
 
 enum PlayerState { Coming, Going }
 
-fn main() {}
+fn main() {
+    let mut player_state = PlayerState::Coming;
+    print_message_for_player_state(player_state);
+    player_state = PlayerState::Going;
+    print_message_for_player_state(player_state);
+}
+
+fn print_message_for_player_state(player_state: PlayerState) {
+    match player_state {
+        PlayerState::Coming => println!("Hello"),
+        PlayerState::Going => println!("Goodbye")
+    }
+}
