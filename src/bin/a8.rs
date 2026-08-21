be7f365e8baa090b04e0a9050f8292d0a0012a63
NonymousMorlock <59588180+NonymousMorlock@users.noreply.github.com>
2026-08-21 21:05:30 +0000

    solve `a8`

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -9,4 +9,43 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum Flavour {
+    Orange,
+    Lemon,
+    Mango,
+    Pineapple,
+    Vanilla,
+    Butterscotch,
+    Mint,
+    Strawberry,
+    Apple,
+}
+
+struct Drink {
+    flavour: Flavour,
+    ounces: f64,
+}
+
+fn flavour_to_string(flavour: &Flavour) -> String {
+    match flavour {
+        Flavour::Orange => "orange".to_string(),
+        Flavour::Lemon => "lemon".to_string(),
+        Flavour::Mango => "mango".to_string(),
+        Flavour::Pineapple => "pineapple".to_string(),
+        Flavour::Vanilla => "vanilla".to_string(),
+        Flavour::Butterscotch => "butterscotch".to_string(),
+        Flavour::Mint => "mint".to_string(),
+        Flavour::Strawberry => "strawberry".to_string(),
+        Flavour::Apple => "apple".to_string()
+    }
+}
+
+fn print_drink(drink: &Drink) {
+    let flavour = flavour_to_string(&drink.flavour);
+    println!("Drink(flavour: {:?}, ounces: {:?})", flavour, drink.ounces);
+}
+
+fn main() {
+    let pina_colada = Drink{flavour: Flavour::Pineapple, ounces: 2.5};
+    print_drink(&pina_colada);
+}
