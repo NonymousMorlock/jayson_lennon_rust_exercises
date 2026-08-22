2ea304352a47aa18614060f32abd7a683c52eb27
NonymousMorlock <59588180+NonymousMorlock@users.noreply.github.com>
2026-08-22 10:15:00 +0000

    fix `a10`

--- src/bin/a10.rs
+++ src/bin/a10.rs
@@ -12,4 +12,24 @@
 // * Use a match expression to determine which message
 //   to print
 
-fn main() {}
+enum Size {
+    Big { message: String },
+    Small { message: String },
+}
+
+fn print_size_message(size: Size) {
+    match size {
+        Size::Big { message } => println!("{message}"),
+        Size::Small { message } => println!("{message}"),
+    }
+}
+
+fn main() {
+    let chunk_size: i32 = 2000;
+    let size_type: Size = if chunk_size > 100 {
+        Size::Big {message: String::from("It's big")}
+    } else {
+        Size::Small {message: String::from("It's small")}
+    };
+    print_size_message(size_type);
+}
