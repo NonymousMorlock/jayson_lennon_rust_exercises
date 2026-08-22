e4e97e544377a118425dfc6866bca11cfe909043
NonymousMorlock <59588180+NonymousMorlock@users.noreply.github.com>
2026-08-22 20:09:06 +0000

    solve `a12`

--- src/bin/a12.rs
+++ src/bin/a12.rs
@@ -10,4 +10,34 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+enum Colour {
+    Green
+}
+
+impl Colour {
+    fn as_string(&self) -> &'static str {
+        match self {
+            Colour::Green => "green"
+        }
+    }
+}
+
+struct ShippingBox {
+    dimensions: String,
+    weight: f64,
+    colour: Colour
+}
+
+impl ShippingBox {
+    fn new(dimensions: String, weight: f64, colour: Colour) -> Self {
+        Self { dimensions, weight, colour}
+    }
+    fn display(&self) {
+        println!("ShippingBox{{dimensions:{:?}, weight: {:?}, color: {:?}}}", self.dimensions, self.weight, self.colour.as_string())
+    }
+}
+
+fn main() {
+    let twenty_ton_box: ShippingBox = ShippingBox::new(String::from("2x2"), 20.0, Colour::Green);
+    twenty_ton_box.display();
+}
