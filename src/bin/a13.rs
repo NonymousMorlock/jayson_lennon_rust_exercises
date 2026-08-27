b4fb839774cb2505f74b0eec885ae2a45ec45593
NonymousMorlock <59588180+NonymousMorlock@users.noreply.github.com>
2026-08-27 23:21:46 +0000

    solve `a13`

--- src/bin/a13.rs
+++ src/bin/a13.rs
@@ -10,4 +10,16 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    let mut numbers: Vec<i32> = Vec::new();
+
+    for i in 1..5 {
+        numbers.push(i * 10);
+    }
+
+    for number in &numbers {
+       println!("{}", if *number != 30 { number.to_string() } else { "thirty".into() });
+    }
+
+    println!("Total number of elements: {:?}", numbers.len())
+}
