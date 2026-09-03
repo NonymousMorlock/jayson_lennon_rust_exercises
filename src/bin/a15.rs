6f796b9882a580b915f795fe1654cf7db4c239c7
NonymousMorlock <59588180+NonymousMorlock@users.noreply.github.com>
2026-09-03 16:17:05 +0000

    solve `a15`

--- src/bin/a15.rs
+++ src/bin/a15.rs
@@ -11,4 +11,39 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+use crate::Ticket::{Backstage, Standard, Vip};
+
+enum Ticket {
+    Backstage { holder_name: String, price: f64 },
+    Vip { holder_name: String, price: f64 },
+    Standard { price: f64 },
+}
+
+fn main() {
+    let mut tickets: Vec<Ticket> = Vec::new();
+    for i in 1..=3 {
+        let ticket: Ticket;
+        if i == 1 {
+            ticket = Backstage {
+                holder_name: "John Doe".to_owned(),
+                price: 1999.99,
+            };
+        } else if i == 2 {
+            ticket = Vip {
+                holder_name: "Albert Donny Einstein".to_owned(),
+                price: 2999.99,
+            };
+        } else {
+            ticket = Standard { price: 99.99 };
+        }
+        tickets.push(ticket);
+    }
+
+    for ticket in &tickets {
+        match ticket {
+            Backstage { holder_name, price } => println!("{}::Backstage::£{}", holder_name, price),
+            Vip { holder_name, price } => println!("{}::Vip::£{}", holder_name, price),
+            Standard { price } => println!("Brokie::Standard::£{}", price),
+        }
+    }
+}
