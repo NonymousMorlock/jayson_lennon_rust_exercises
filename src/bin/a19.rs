9111688791c69e5bd7ba32f7803d5ddd03f78370
NonymousMorlock <59588180+NonymousMorlock@users.noreply.github.com>
2026-09-09 02:16:01 +0000

    solve `a19`

--- src/bin/a19.rs
+++ src/bin/a19.rs
@@ -13,4 +13,66 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
+
+struct Furniture {
+    name: String,
+    quantity: i32,
+}
+
+fn main() {
+    let furniture_database: HashMap<i32, Furniture> = HashMap::from([
+        (
+            1,
+            Furniture {
+                name: "Chair".to_owned(),
+                quantity: 5,
+            },
+        ),
+        (
+            2,
+            Furniture {
+                name: "Bed".to_owned(),
+                quantity: 3,
+            },
+        ),
+        (
+            3,
+            Furniture {
+                name: "Table".to_owned(),
+                quantity: 2,
+            },
+        ),
+        (
+            4,
+            Furniture {
+                name: "Couch".to_owned(),
+                quantity: 0,
+            },
+        ),
+    ]);
+
+    let mut total_number_of_items: i32 = 0;
+
+    for furniture in furniture_database.values() {
+        if furniture.quantity < 0 {
+            println!(
+                "Something went wrong. Furniture with name {} has a negative quantity - {}",
+                furniture.name, furniture.quantity
+            );
+            return;
+        }
+        println!(
+            "{} -- {}",
+            furniture.name,
+            if furniture.quantity > 0 {
+                furniture.quantity.to_string()
+            } else {
+                "out of stock".to_owned()
+            }
+        );
+        total_number_of_items += furniture.quantity;
+    }
+
+    println!("Total number of items in stock: {}", total_number_of_items);
+}
